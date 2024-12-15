7923dec0049923e3922c8aff4f7109f645d00acd
Shantanu Kulkarni <shantanukulkarni1229@gmail.com>
2024-12-15 23:58:50 +0530

    `Update .gitignore, remove LICENSE file, and update app/layout.tsx`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import TanStackProvider from "@/components/providers/TanStackProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Stripe Subscriptions",
+	title: "PayVerse",
 	description: "Learn how to integrate Stripe subscriptions with Next.js",
 };
 
